c034086ffc117e6dae1a4c930a8e7eac9677d40a
karthikkm007 <jacksparrow941994@gmail.com>
2018-06-17 19:55:57 +0530

    wt88047: enable adb on boot for 64 bit

--- device.mk
+++ device.mk
@@ -519,4 +519,6 @@ PRODUCT_ENFORCE_RRO_TARGETS := framework-res
 # Debug
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     ro.adb.secure=0 \
-    ro.secure=0
+    ro.secure=0 \
+    persist.sys.usb.config=adb,mtp \
+    ro.debugable=1
