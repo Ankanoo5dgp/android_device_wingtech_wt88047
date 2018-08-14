c132cd5d95c997b2cff5cce0ed5fd9860c44a796
karthikkm007 <jacksparrow941994@gmail.com>
2018-08-14 23:52:35 +0530

    Revert "wt88047: use Prebuilt kernel"
    This reverts commit f772a1e33b7e3a9441aac388473a691c4648a572.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -115,10 +115,9 @@ TARGET_RECOVERY_DEVICE_MODULES := libinit_msm8916
 TARGET_LIBINIT_MSM8916_DEFINES_FILE := $(DEVICE_PATH)/init/init_wt88047.cpp
 
 # Kernel
-TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/zImage-dtb
-#BOARD_DTBTOOL_ARGS := -2
-#BOARD_KERNEL_IMAGE_NAME := Image-dtb
-#BOARD_KERNEL_SEPARATED_DT := true
+BOARD_DTBTOOL_ARGS := -2
+BOARD_KERNEL_IMAGE_NAME := zImage-dtb
+BOARD_KERNEL_SEPARATED_DT := false
 TARGET_KERNEL_SOURCE := kernel/wingtech/msm8916
 TARGET_KERNEL_CONFIG := excalibur_defconfig
 BOARD_KERNEL_CMDLINE += sched_enable_hmp=1 phy-msm-usb.floated_charger_enable=1 console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1 androidboot.selinux=permissive
