d04269d67ac2651412c53ccdb3da38811fe50edc
karthikkm007 <jacksparrow941994@gmail.com>
2018-08-15 00:14:50 +0530

    wt88047: do not prebuilt kernel

--- slim.mk
+++ slim.mk
@@ -35,3 +35,10 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="marlin-user 8.1.0 OPM4.171019.021.P1 4820305 release-keys"
 
 BUILD_FINGERPRINT := google/marlin/marlin:8.1.0/OPM4.171019.021.P1/4820305:user/release-keys
+<<<<<<< HEAD
+=======
+
+#Prebult kernel
+#PRODUCT_COPY_FILES += \
+#    $(LOCAL_PATH)/zImage-dtb:kernel 
+>>>>>>> 9df0c0e... wt88047: do not prebuilt kernel
